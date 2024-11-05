0ed036f34231a35115b363943b84883ebe675147
anoushka <vyasanoushka@gmail.com>
2024-11-05 23:30:09 +0530

    hero page and navbar done

---
+++ sanity/schemaTypes/heroImages.ts
@@ -0,0 +1,23 @@
+import { title } from "process";
+
+export default {
+  name:'heroImg',
+  type:'document',
+  title:' Two Hero Images',
+  fields:[
+    {
+      name:'image1',
+      type:'image',
+      title:'First Image'
+    },
+    {
+      name:'image2',
+      type:'image',
+      title:'Second image',
+     
+    }
+,
+   
+    
+  ]
+}
